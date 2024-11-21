fd7050d702bf837172e1b087a2b1c5a16374d04e
Manuel Goetschi <manuel.goetschi@innosolv.ch>
2024-11-21 13:34:56 +0100

    feat: Weitere Fälle

--- Interview.Tests/Question1.Tests.cs
+++ Interview.Tests/Question1.Tests.cs
@@ -2,16 +2,32 @@ using FluentAssertions;
 
 namespace Interview.Tests;
 
-public class UnitTest1
+public class QuestionsTests
 {
     [Theory]
     [InlineData("hello", "olleh")]
     [InlineData("Hello World!", "!dlroW olleH")]
     [InlineData("step on no pets", "step on no pets")]
-    public void Question1_ReverseString_Should_Reverse_InputString(string input, string expected)
+    public void ReverseString_Should_Reverse_InputString(string input, string expected)
     {
-        var result = Question1.RevervseString(input);
+        var result = Questions.RevervseString(input);
 
         result.Should().Be(expected);
     }
+
+    [Theory]
+    [MemberData(nameof(CharData))]
+    public void CharCount_Should_Count_Each_Char(string input, Dictionary<char, int> expected)
+    {
+        var result = Questions.CharCount(input);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+    
+    public static IEnumerable<object[]> CharData =>
+        [
+            ["Hello", new Dictionary<char, int>() { { 'h', 1 }, { 'e', 1 }, { 'l', 2 }, { 'o', 1 }, }],
+            ["Starkes Team", new Dictionary<char, int>() { { 's', 2 }, { 't', 2 }, { 'a', 2 }, { 'r', 1 }, { 'k', 1 }, { 'e', 2 }, { 'm', 1 }, }],
+        ];
+    
 }
---
+++ Interview/Questions.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Interview;
+
+public static class Questions
+{
+    public static string RevervseString(string str){
+
+       //TODO: Implement 
+       return "";
+    }
+
+
+    public static IDictionary<char, int> CharCount(string str)
+    {
+        // TODO: Implement
+        return new Dictionary<char, int>();
+    }
+}
+
+
+// Testumgebung für Visual Regression Tests
+// - Alle Angular Komponenten sind in Storybook hinterlegt
+// - Die Tests sollen mit Playwright ausgeführt werden
+
+// -> Ziel: Eine Umgebung aufbauen, welche jede einzelne Komponente auf die visuelle Korrektheit prüft
